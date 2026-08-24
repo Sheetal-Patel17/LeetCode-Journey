3af2a654b8df85ef81777ba32a617e717285aa70
Sheetal Patel <patelsheetal670@gmail.com>
2026-08-24 12:49:59 +0530

    Add sorting problems

---
+++ Sorting/Kth_Smallest_Element.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {7, 10, 4, 3, 20, 15};
+    int n = 6;
+    int k = 3;
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Kth Smallest Element: " << arr[k - 1] << endl;
+
+    return 0;
+}
---
+++ Sorting/Merge_Sort.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int left, int mid, int right) {
+    int temp[100];
+    int i = left;
+    int j = mid + 1;
+    int k = left;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid)
+        temp[k++] = arr[i++];
+
+    while (j <= right)
+        temp[k++] = arr[j++];
+
+    for (i = left; i <= right; i++)
+        arr[i] = temp[i];
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left >= right)
+        return;
+
+    int mid = (left + right) / 2;
+
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+}
+
+int main() {
+    int arr[] = {38, 27, 43, 3, 9, 82, 10};
+    int n = 7;
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted Array: ";
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    cout << endl;
+
+    return 0;
+}
