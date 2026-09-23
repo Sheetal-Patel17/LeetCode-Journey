aaa6d16b83c687ef0439d6e00e2776a45b8f8ab3
Sheetal Patel <patelsheetal670@gmail.com>
2026-09-23 09:09:15 +0530

    Improve missing number solution

--- Bit_Manipulation/Find_Missing_Number.cpp
+++ Bit_Manipulation/Find_Missing_Number.cpp
@@ -1,19 +1,30 @@
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
-int main() {
-    int arr[] = {1, 2, 4, 5, 6};
-    int n = 6;
+int main()
+{
+    int n;
 
-    int result = 0;
+    cout << "Enter n: ";
+    cin >> n;
 
-    for (int i = 1; i <= n; i++)
-        result = result ^ i;
+    int xorValue = 0;
 
-    for (int i = 0; i < n - 1; i++)
-        result = result ^ arr[i];
+    for (int i = 0; i <= n; i++)
+    {
+        xorValue = xorValue ^ i;
+    }
 
-    cout << "Missing Number: " << result;
+    cout << "Enter " << n << " numbers from 0 to " << n << ":" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        int value;
+        cin >> value;
+        xorValue = xorValue ^ value;
+    }
+
+    cout << "Missing number: " << xorValue;
 
     return 0;
 }
